1a0233a4208daddda5fc1c3d598b2f68a1897cf3
mohammadsofan <mohammadsof72@gmail.com>
2025-08-04 16:58:36 +0300

    feat(User): add User model, Login, Register Dtos

---
+++ SimpleAuthAPI/Dtos/UserRegisterRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAuthAPI.Dtos
+{
+    public class UserRegisterRequestDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
