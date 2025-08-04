88790835979f6d5ab3b70738afefbce6f3c339cc
mohammadsofan <mohammadsof72@gmail.com>
2025-08-04 17:08:58 +0300

    feat(User): Add Users controller and apply authorization on the controller and it's endpoints

---
+++ SimpleAuthAPI/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAuthAPI.Data;
+
+namespace SimpleAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAllUSers()
+        {
+            return Ok(new { Users = FakeUserStorage.Users });
+        }
+        [HttpGet("LetMeIn")]
+        public IActionResult LetMeIn()
+        {
+            return Ok("welcome, you are authorized here!");
+        }
+    }
+}
